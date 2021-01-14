3e4fa5723677cf5f7d5dd0769dc3d7a47c1bc205
EC2 Default User <ec2-user@ip-172-31-10-226.ec2.internal>
2021-01-14 11:33:23 +0000

    Rails Fin

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -11,15 +11,40 @@ class TodolistsController < ApplicationController
     list = List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    # ３. トップ画面へリダイレクト　←　変更
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
   end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  def destroy
+   list = List.find(params[:id]) # データ（レコード）を1件取得
+   list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト  end
+  end
+
 
   private
   # ストロングパラメータ↑のprivateはコントローラー内でのみの命令でアクションを起こさない。
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
+
 
 end
